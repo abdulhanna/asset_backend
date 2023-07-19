f6bbe391a7833b2ff8d27db3dadd0c5948d6fc24
Dhananjay Kumar <dhananjay@plaxonic.com>
2023-07-19 15:34:07 +0530

    Fix indentation

--- src/modules/auth/router/auth.js
+++ src/modules/auth/router/auth.js
@@ -1,41 +1,38 @@
-import { Router } from "express";
-import { httpHandler } from "@madhouselabs/http-helpers";
-import StatusCodes from "http-status-codes";
+import { Router } from 'express';
+import { httpHandler } from '@madhouselabs/http-helpers';
+import StatusCodes from 'http-status-codes';
 import { isLoggedIn } from './passport.js';
-import authService from "../services/auth";
-
+import authService from '../services/auth';
 
 const router = Router();
 
 router.post(
-    "/register",
-    httpHandler(async (req, res) => {
-      const result = await authService.doRegister(req.body);
-      res.send(result);
-    })
-  );
-
+     '/register',
+     httpHandler(async (req, res) => {
+          const result = await authService.doRegister(req.body);
+          res.send(result);
+     })
+);
 
 // Token verify on email
 router.get(
-  "/confirm/:token",
-  httpHandler(async (req, res) => {
-    const { token } = req.params;
-    const result = await authService.verifyUser(token);
-    res.send(result);
-  })
+     '/confirm/:token',
+     httpHandler(async (req, res) => {
+          const { token } = req.params;
+          const result = await authService.verifyUser(token);
+          res.send(result);
+     })
 );
-
 
 // profile complete
 
 router.get(
-  "/profileComplete/:token",
-  httpHandler(async (req, res) => {
-    const { token } = req.params;
-    const result = await authService.verifyUser(token);
-    res.send(result);
-  })
-)
+     '/profileComplete/:token',
+     httpHandler(async (req, res) => {
+          const { token } = req.params;
+          const result = await authService.verifyUser(token);
+          res.send(result);
+     })
+);
 
 export default router;
