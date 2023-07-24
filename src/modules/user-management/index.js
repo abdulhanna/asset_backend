7ced6680741cd222e5c9b2770c53182abe87e631
Dhananjay Kumar <dhananjay@plaxonic.com>
2023-07-24 11:33:22 +0530

    Add userMember routers in user management

--- src/modules/user-management/index.js
+++ src/modules/user-management/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import userManagementRouter from './router/permissions.js';
 import userRoleRouter from './router/roles.js';
+import membersRouter from './router/userMember.js';
 
 const router = Router();
-router.use('/user-management', userManagementRouter);
+router.use('/user-management/permissions', userManagementRouter);
 router.use('/user-management/roles', userRoleRouter);
+router.use('/user-management/members', membersRouter);
 
 const userManagementModule = {
      init: (app) => {
---
+++ src/modules/user-management/services/userMember.js
@@ -0,0 +1,75 @@
+import jwtService from '../../auth/services/jwt-services';
+import emailtemplate from '../../../helpers/send-email';
+import userModel from '../../auth/models/index.js';
+
+const createMember = async (userData) => {
+     try {
+          // Generate a verification token
+          const verificationTokenPayload = await jwtService.generatePair(
+               userData.email
+          );
+          const verificationToken = verificationTokenPayload.access_token;
+
+          // Send the invitation email to the member
+          await emailtemplate.sendInvitationEmail(
+               userData.email,
+               verificationToken
+          );
+
+          // Save the member with the verification token to the database
+          const member = new userModel({
+               email: userData.email,
+               password: userData.password,
+               parentId: userData.parentId,
+               teamrole: userData.teamrole,
+               userProfile: userData.userProfile,
+               verificationToken: verificationToken,
+          });
+
+          const savedMember = await member.save();
+
+          return savedMember;
+     } catch (error) {
+          console.log(error);
+          throw new Error('Failed to create member');
+     }
+};
+
+const getAllMembers = async (parentId) => {
+     try {
+          const members = await userModel
+               .find({ parentId })
+               .populate('teamrole');
+          return members;
+     } catch (error) {
+          console.log(error);
+          throw new Error('Failed to fetch members');
+     }
+};
+
+const setPassword = async (verificationToken, password) => {
+     try {
+          // Find the member using the verification token
+          const member = await userModel.findOne({ verificationToken });
+
+          if (!member) {
+               return { success: false };
+          }
+
+          // Set the new password
+          member.password = password;
+          member.verificationToken = null;
+          await member.save();
+
+          return { success: true };
+     } catch (error) {
+          console.log(error);
+          throw new Error('Failed to set password');
+     }
+};
+
+export const memberService = {
+     createMember,
+     getAllMembers,
+     setPassword,
+};
