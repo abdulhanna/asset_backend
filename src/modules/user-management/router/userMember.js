5da7c1dedd40435c510b97df6f5f519fffe58563
Dhananjay Kumar <dhananjay@plaxonic.com>
2023-07-27 15:32:08 +0530

    Add pdf router for members list by parentId

--- src/modules/user-management/router/userMember.js
+++ src/modules/user-management/router/userMember.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { memberService } from '../services/userMember.js';
 import { isLoggedIn } from '../../auth/router/passport.js';
+import PDFDocument from 'pdfkit';
 
 const router = Router();
 // Create a new member
@@ -85,5 +86,140 @@ router.get('/:parentId', async (req, res) => {
           res.status(500).json({ success: false, error: error.message });
      }
 });
+
+//------------------------
+router.get('/:parentId/download', async (req, res) => {
+     try {
+          const { parentId } = req.params;
+          const members = await memberService.getAllMembers(parentId);
+
+          // Get the desired format from query parameter (pdf, csv, xlsx)
+          const format = req.query.format;
+
+          if (format === 'pdf') {
+               // Generate and send PDF
+               const pdfBuffer = await generatePDF(members);
+               res.setHeader(
+                    'Content-Disposition',
+                    'attachment; filename=members.pdf'
+               );
+               res.setHeader('Content-Type', 'application/pdf');
+               res.send(pdfBuffer);
+          } else if (format === 'csv') {
+               // Generate and send CSV
+               const csvBuffer = await generateCSV(members);
+               res.setHeader(
+                    'Content-Disposition',
+                    'attachment; filename=members.csv'
+               );
+               res.setHeader('Content-Type', 'text/csv');
+               res.send(csvBuffer);
+          } else if (format === 'xlsx') {
+               // Generate and send XLSX
+               const xlsxBuffer = await generateXLSX(members);
+               res.setHeader(
+                    'Content-Disposition',
+                    'attachment; filename=members.xlsx'
+               );
+               res.setHeader(
+                    'Content-Type',
+                    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+               );
+               res.send(xlsxBuffer);
+          } else {
+               throw new Error('Invalid format specified.');
+          }
+     } catch (error) {
+          res.status(500).json({ success: false, error: error.message });
+     }
+});
+
+const extractMembersData = (members) => {
+     return members.map((member, index) => ({
+          index: index + 1,
+          roleName: member.teamrole.roleName || 'N/A',
+          name: member.userProfile.name || 'N/A',
+          email: member.email || 'N/A',
+          phone: member.userProfile.phone || 'N/A',
+          createdAt: member.createdAt || 'N/A',
+     }));
+};
+
+const generatePDF = async (members) => {
+     const membersData = extractMembersData(members);
+     return new Promise((resolve, reject) => {
+          const doc = new PDFDocument();
+          const chunks = [];
+
+          doc.on('data', (chunk) => {
+               chunks.push(chunk);
+          });
+
+          doc.on('end', () => {
+               resolve(Buffer.concat(chunks));
+          });
+
+          doc.on('error', (error) => {
+               reject(error);
+          });
+
+          doc.fontSize(14).text('List of Members', { align: 'center' });
+          doc.fontSize(12).text('Parent ID: ' + members[0].parentId, {
+               align: 'center',
+          });
+          doc.moveDown();
+
+          membersData.forEach((member) => {
+               doc.text(`${member.index}. ${member.name}`);
+               doc.text(`${member.index}. ${member.roleName}`);
+               doc.text(`${member.index}. ${member.email}`);
+               doc.text(`${member.index}. ${member.phone}`);
+          });
+
+          doc.end();
+     });
+};
+
+const generateCSV = async (members) => {
+     const membersData = extractMembersData(members);
+
+     return new Promise((resolve) => {
+          const chunks = [];
+          const csvStream = fastcsv.format({ headers: true });
+
+          csvStream.pipe(
+               concatStream((data) => {
+                    resolve(Buffer.concat(chunks));
+               })
+          );
+
+          membersData.forEach((member) => {
+               csvStream.write({ Name: member.name });
+          });
+
+          csvStream.end();
+          csvStream.on('data', (chunk) => {
+               chunks.push(chunk);
+          });
+     });
+};
+
+const generateXLSX = async (members) => {
+     const membersData = extractMembersData(members);
+     return new Promise((resolve) => {
+          const workbook = new ExcelJS.Workbook();
+          const worksheet = workbook.addWorksheet('Members');
+
+          worksheet.columns = [{ header: 'Name', key: 'name', width: 20 }];
+
+          membersData.forEach((member) => {
+               worksheet.addRow({ name: member.name });
+          });
+
+          workbook.xlsx.writeBuffer().then((buffer) => {
+               resolve(buffer);
+          });
+     });
+};
 
 export default router;
