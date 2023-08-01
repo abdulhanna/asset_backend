cd067f07e2fd1d0469e193968de846b814f1e17d
Dhananjay Kumar <dhananjay@plaxonic.com>
2023-08-01 11:19:44 +0530

    Add departments module api's

--- src/modules/organization/router/departments.js
+++ src/modules/organization/router/departments.js
@@ -6,11 +6,11 @@ const router = express.Router();
 router.post('/', async (req, res) => {
      try {
           const data = req.body;
-          const department = await departmentService.addDepartment(data);
+          const addedDepartment = await departmentService.addDepartment(data);
           res.status(201).json({
                success: true,
                msg: 'Departments added successfully',
-               department,
+               department: addedDepartment,
           });
      } catch (error) {
           console.log(error);
@@ -19,6 +19,44 @@ router.post('/', async (req, res) => {
                error: 'Unable to add departments',
           });
      }
+});
+
+router.put('/:id', async (req, res) => {
+     try {
+          const id = req.params.id;
+          const data = req.body;
+          const updatedDepartment = await departmentService.updateDepartment(
+               id,
+               data
+          );
+          res.status(200).json({
+               success: true,
+               msg: 'Department updated successfully',
+               department: updatedDepartment,
+          });
+     } catch (error) {
+          console.log(error);
+          res.status(500).json({
+               success: false,
+               error: 'Unable to update department',
+          });
+     }
+});
+
+router.get('/:id', async (req, res) => {
+     try {
+          const id = req.params.id;
+          const department = await departmentService.getDepartmentById(id);
+          res.status(200).json({
+               success: true,
+               department,
+          });
+     } catch (error) {
+          res.status(500).json({
+               success: false,
+               error: 'Unable to fetch department',
+          });
+     }g
 });
 
 router.get('/', async (req, res) => {
@@ -33,6 +71,22 @@ router.get('/', async (req, res) => {
           res.status(500).json({
                success: false,
                error: 'Unable to fetch departments',
+          });
+     }
+});
+
+router.delete('/:id', async (req, res) => {
+     try {
+          const id = req.params.id;
+          await departmentService.deleteDepartment(id);
+          res.status(200).json({
+               success: true,
+               msg: 'Department deleted successfully',
+          });
+     } catch (error) {
+          res.status(500).json({
+               success: false,
+               error: 'Unable to delete department',
           });
      }
 });
--- src/modules/organization/services/departments.js
+++ src/modules/organization/services/departments.js
@@ -2,22 +2,60 @@ import departmentModel from '../models/departments';
 
 const addDepartment = async (data) => {
      try {
-          return await departmentModel.create(data);
+          const addDepartment = await departmentModel.create(data);
+
+          return addDepartment;
      } catch (error) {
           throw new Error('Unable to add departments');
      }
 };
 
+const updateDepartment = async (id, data) => {
+     try {
+          const updatedDepartment = await departmentModel.findByIdAndUpdate(
+               id,
+               data,
+               { new: true } // This option returns the updated document
+          );
+          return updatedDepartment;
+     } catch (error) {
+          throw new Error('Unable to update department');
+     }
+};
+
+const getDepartmentById = async (id) => {
+     try {
+          const department = await departmentModel.findById(id);
+          if (!department) {
+               throw new Error('Department not found');
+          }
+          return department;
+     } catch (error) {
+          throw new Error('Unable to fetch department');
+     }
+};
+
 const getDepartments = async () => {
      try {
-          const data = await departmentModel.find();
+          const data = await departmentModel.find().select('-__v');
           return data;
      } catch (error) {
           throw new Error('Unable to fetch departments');
      }
 };
 
+const deleteDepartment = async (id) => {
+     try {
+          await departmentModel.findByIdAndDelete(id);
+     } catch (error) {
+          throw new Error('Unable to delete department');
+     }
+};
+
 export const departmentService = {
      addDepartment,
+     updateDepartment,
+     getDepartmentById,
      getDepartments,
+     deleteDepartment,
 };
