6013e3dd2b7b8077248c0d4cc16a6fc1521f99e5
Dhananjay Kumar <dhananjay@plaxonic.com>
2023-08-25 16:48:23 +0530

    Add dashboardType in permission list

--- src/modules/user-management/router/permissions.js
+++ src/modules/user-management/router/permissions.js
@@ -1,182 +1,183 @@
 import express from 'express';
-import { permissionService } from '../services/permissions.js';
-import { isLoggedIn } from '../../auth/router/passport.js';
+import {permissionService} from '../services/permissions.js';
+import {isLoggedIn} from '../../auth/router/passport.js';
 import permissionModel from '../models/permissions.js';
 import mongoose from 'mongoose';
 
 const router = express.Router();
 
 router.post('/create', isLoggedIn, async (req, res) => {
-     try {
-          let { moduleName, read, readWrite, actions,dashboardType } = req.body;
+    try {
+        let {moduleName, read, readWrite, actions, dashboardType} = req.body;
 
-          // Custom validation for the create permission request
-          if (!moduleName || moduleName.trim() === '') {
-               return res.status(400).json({
-                    success: false,
-                    error: 'Invalid request data. moduleName is required and should not be empty.',
-               });
-          }
+        // Custom validation for the create permission request
+        if (!moduleName || moduleName.trim() === '') {
+            return res.status(400).json({
+                success: false,
+                error: 'Invalid request data. moduleName is required and should not be empty.',
+            });
+        }
 
-          // Normalize the moduleName (remove white spaces and convert to lowercase)
-          moduleName = moduleName.trim().toLowerCase();
+        // Normalize the moduleName (remove white spaces and convert to lowercase)
+        moduleName = moduleName.trim().toLowerCase();
 
-          // Check if the normalized moduleName already exists
-          const existingModuleName = await permissionModel.findOne({
-               moduleName,
-               isDeactivated: false,
-          });
+        // Check if the normalized moduleName already exists
+        const existingModuleName = await permissionModel.findOne({
+            moduleName,
+            isDeactivated: false,
+        });
 
-          if (existingModuleName) {
-               return res.status(400).json({
-                    success: false,
-                    error: `Module with the name '${moduleName}' already exists.`,
-               });
-          }
+        if (existingModuleName) {
+            return res.status(400).json({
+                success: false,
+                error: `Module with the name '${moduleName}' already exists.`,
+            });
+        }
 
-          const permissionData = {
-               moduleName,
-               read,
-               readWrite,
-               actions,
-               dashboardType,
-               createdAt: new Date(),
-          };
+        const permissionData = {
+            moduleName,
+            read,
+            readWrite,
+            actions,
+            dashboardType,
+            createdAt: new Date(),
+        };
 
-          const permission = await permissionService.createPermission(
-               permissionData
-          );
+        const permission = await permissionService.createPermission(
+            permissionData
+        );
 
-          return res.status(201).json({ success: true, permission });
-     } catch (error) {
-          return res.status(500).json({ success: false, error: error.message });
-     }
+        return res.status(201).json({success: true, permission});
+    } catch (error) {
+        return res.status(500).json({success: false, error: error.message});
+    }
 });
 
 const isValidObjectId = (id) => {
-     return mongoose.Types.ObjectId.isValid(id);
+    return mongoose.Types.ObjectId.isValid(id);
 };
 
 router.put('/update/:id', async (req, res) => {
-     try {
-          const { id } = req.params;
-          const updateData = req.body;
+    try {
+        const {id} = req.params;
+        const updateData = req.body;
 
-          // Custom validation for the update permission request
-          if (!isValidObjectId(id)) {
-               return res.status(400).json({
-                    success: false,
-                    error: 'Invalid permission ID',
-               });
-          }
+        // Custom validation for the update permission request
+        if (!isValidObjectId(id)) {
+            return res.status(400).json({
+                success: false,
+                error: 'Invalid permission ID',
+            });
+        }
 
-          // Check if moduleName is provided and is empty during update
-          if (
-               'moduleName' in updateData &&
-               typeof updateData.moduleName === 'string' &&
-               updateData.moduleName.trim() === ''
-          ) {
-               return res.status(400).json({
-                    success: false,
-                    error: 'moduleName should not be empty.',
-               });
-          }
+        // Check if moduleName is provided and is empty during update
+        if (
+            'moduleName' in updateData &&
+            typeof updateData.moduleName === 'string' &&
+            updateData.moduleName.trim() === ''
+        ) {
+            return res.status(400).json({
+                success: false,
+                error: 'moduleName should not be empty.',
+            });
+        }
 
-          const permission = await permissionService.updatePermission(
-               id,
-               updateData
-          );
+        const permission = await permissionService.updatePermission(
+            id,
+            updateData
+        );
 
-          if (!permission) {
-               return res
-                    .status(404)
-                    .json({ success: false, error: 'Permission not found' });
-          }
+        if (!permission) {
+            return res
+                .status(404)
+                .json({success: false, error: 'Permission not found'});
+        }
 
-          return res.json({ success: true, permission });
-     } catch (error) {
-          return res.status(500).json({ success: false, error: error.message });
-     }
+        return res.json({success: true, permission});
+    } catch (error) {
+        return res.status(500).json({success: false, error: error.message});
+    }
 });
 
 router.get('/all', async (req, res) => {
-     try {
-          const permissions = await permissionService.getAllPermissions();
-          return res.status(200).json({ success: true, permissions });
-     } catch (error) {
-          return res.status(500).json({ success: false, error: error.message });
-     }
+    try {
+        const permissions = await permissionService.getAllPermissions();
+        return res.status(200).json({success: true, permissions});
+    } catch (error) {
+        return res.status(500).json({success: false, error: error.message});
+    }
 });
 
 router.delete('/v1/:id', async (req, res) => {
-     try {
-          const { id } = req.params;
+    try {
+        const {id} = req.params;
 
-          // Custom validation for the update permission request
-          if (!isValidObjectId(id)) {
-               return res.status(400).json({
-                    success: false,
-                    error: 'Invalid permission ID',
-               });
-          }
+        // Custom validation for the update permission request
+        if (!isValidObjectId(id)) {
+            return res.status(400).json({
+                success: false,
+                error: 'Invalid permission ID',
+            });
+        }
 
-          const existingPermission = await permissionModel.findById(id);
-          if (!existingPermission) {
-               res.status(404).json({
-                    success: false,
-                    error: 'moduleName not found',
-               });
-          }
+        const existingPermission = await permissionModel.findById(id);
+        if (!existingPermission) {
+            res.status(404).json({
+                success: false,
+                error: 'moduleName not found',
+            });
+        }
 
-          const deletePermissions =
-               await permissionService.hardDeletePermissions(id);
-          return res.status(200).json({
-               success: true,
-               msg: 'Deleted successfully',
-               deletePermissions,
-          });
-     } catch (error) {
-          return res.status(500).json({
-               success: false,
-               error: error.message,
-          });
-     }
+        const deletePermissions =
+            await permissionService.hardDeletePermissions(id);
+        return res.status(200).json({
+            success: true,
+            msg: 'Deleted successfully',
+            deletePermissions,
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            error: error.message,
+        });
+    }
 });
 
 router.delete('/v2/:id', async (req, res) => {
-     try {
-          const { id } = req.params;
+    try {
+        const {id} = req.params;
 
-          // Custom validation for the update permission request
-          if (!isValidObjectId(id)) {
-               return res.status(400).json({
-                    success: false,
-                    error: 'Invalid permission ID',
-               });
-          }
+        // Custom validation for the update permission request
+        if (!isValidObjectId(id)) {
+            return res.status(400).json({
+                success: false,
+                error: 'Invalid permission ID',
+            });
+        }
 
-          const existingPermission = await permissionModel.findById(id);
-          if (!existingPermission) {
-               res.status(404).json({
-                    success: false,
-                    error: 'moduleName not found',
-               });
-          }
+        const existingPermission = await permissionModel.findById(id);
+        if (!existingPermission) {
+            res.status(404).json({
+                success: false,
+                error: 'moduleName not found',
+            });
+        }
 
-          const deletePermissions =
-               await permissionService.softDeletePermissions(id);
+        const deletePermissions =
+            await permissionService.softDeletePermissions(id);
 
-          return res.status(200).json({
-               success: true,
-               msg: 'Soft deleted successfully',
-               deletePermissions,
-          });
-     } catch (error) {
-          return res.status(500).json({
-               success: false,
-               error: error.message,
-          });
-     }
+        return res.status(200).json({
+            success: true,
+            msg: 'Soft deleted successfully',
+            deletePermissions,
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            error: error.message,
+        });
+    }
 });
+
 
 export default router;
--- src/modules/user-management/services/permissions.js
+++ src/modules/user-management/services/permissions.js
@@ -1,94 +1,94 @@
-import { permissionModel } from '../models';
+import {permissionModel} from '../models';
 
 const createPermission = async (permissionData) => {
-     try {
-          const permission = await permissionModel.create(permissionData);
+    try {
+        const permission = await permissionModel.create(permissionData);
 
-          return permission;
-     } catch (error) {
-          throw new Error(error.message);
-     }
+        return permission;
+    } catch (error) {
+        throw new Error(error.message);
+    }
 };
 
 const updatePermission = async (id, updateData) => {
-     try {
-          const existingPermission = await permissionModel.findById(id);
+    try {
+        const existingPermission = await permissionModel.findById(id);
 
-          if (!existingPermission) {
-               const error = new Error('Permission not found');
-               error.statusCode = 404;
-          }
+        if (!existingPermission) {
+            const error = new Error('Permission not found');
+            error.statusCode = 404;
+        }
 
-          const updatedData = {
-               updatedAt: new Date(),
-               ...updateData,
-          };
+        const updatedData = {
+            updatedAt: new Date(),
+            ...updateData,
+        };
 
-          // Handle access flags
-          if (updateData.allAccess) {
-               updatedData.allAccess = true;
-               updatedData.removeAccess = false;
-               updatedData.restoreDefaults = false;
-               updatedData.read = true;
-               updatedData.readWrite = true;
-               updatedData.actions = true;
-          } else if (updateData.removeAccess) {
-               updatedData.allAccess = false;
-               updatedData.removeAccess = true;
-               updatedData.restoreDefaults = false;
-               updatedData.read = false;
-               updatedData.readWrite = false;
-               updatedData.actions = false;
-          } else if (updateData.restoreDefaults) {
-               updatedData.allAccess = false;
-               updatedData.removeAccess = false;
-               updatedData.restoreDefaults = true;
-          }
+        // Handle access flags
+        if (updateData.allAccess) {
+            updatedData.allAccess = true;
+            updatedData.removeAccess = false;
+            updatedData.restoreDefaults = false;
+            updatedData.read = true;
+            updatedData.readWrite = true;
+            updatedData.actions = true;
+        } else if (updateData.removeAccess) {
+            updatedData.allAccess = false;
+            updatedData.removeAccess = true;
+            updatedData.restoreDefaults = false;
+            updatedData.read = false;
+            updatedData.readWrite = false;
+            updatedData.actions = false;
+        } else if (updateData.restoreDefaults) {
+            updatedData.allAccess = false;
+            updatedData.removeAccess = false;
+            updatedData.restoreDefaults = true;
+        }
 
-          return await permissionModel.findByIdAndUpdate(id, updatedData, {
-               new: true,
-          });
-     } catch (error) {
-          throw new Error(error.message);
-     }
+        return await permissionModel.findByIdAndUpdate(id, updatedData, {
+            new: true,
+        });
+    } catch (error) {
+        throw new Error(error.message);
+    }
 };
 
 const getAllPermissions = async () => {
-     try {
-          const permissions = await permissionModel
-               .find({ isDeleted: false, isDeactivated: false })
-               .select('moduleName read readWrite actions allAccess removeAccess _id');
-          return permissions;
-     } catch (error) {
-          throw new Error(error.message);
-     }
+    try {
+        const permissions = await permissionModel
+            .find({isDeleted: false, isDeactivated: false})
+            .select('moduleName read readWrite actions allAccess removeAccess dashboardType _id');
+        return permissions;
+    } catch (error) {
+        throw new Error(error.message);
+    }
 };
 
 const softDeletePermissions = async (id) => {
-     try {
-          const softDeleteResult = await permissionModel.updateOne(
-               { _id: id },
-               { isDeactivated: true }
-          );
-          return softDeleteResult;
-     } catch (error) {
-          throw new Error('Error in soft deleting resource');
-     }
+    try {
+        const softDeleteResult = await permissionModel.updateOne(
+            {_id: id},
+            {isDeactivated: true}
+        );
+        return softDeleteResult;
+    } catch (error) {
+        throw new Error('Error in soft deleting resource');
+    }
 };
 
 const hardDeletePermissions = async (id) => {
-     try {
-          const hardDeleteResult = await permissionModel.deleteOne({ _id: id });
-          return hardDeleteResult;
-     } catch (error) {
-          throw new Error('Error in hard deleting resource');
-     }
+    try {
+        const hardDeleteResult = await permissionModel.deleteOne({_id: id});
+        return hardDeleteResult;
+    } catch (error) {
+        throw new Error('Error in hard deleting resource');
+    }
 };
 
 export const permissionService = {
-     createPermission,
-     updatePermission,
-     getAllPermissions,
-     softDeletePermissions,
-     hardDeletePermissions,
+    createPermission,
+    updatePermission,
+    getAllPermissions,
+    softDeletePermissions,
+    hardDeletePermissions,
 };
