ad0e024dba6212074bc1fbad4e0ea913362eca8c
Dhananjay Kumar <dhananjay@plaxonic.com>
2023-09-14 22:03:02 +0530

    Update cors function to allow cors origin

--- src/app.js
+++ src/app.js
@@ -2,40 +2,50 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import { errorHandler, notFoundHandler } from './helpers/express-middleware';
+import {errorHandler, notFoundHandler} from './helpers/express-middleware';
 import authModule from './modules/auth';
 import userManagementModule from './modules/user-management';
 import organizationModule from './modules/organization';
 import fieldManagementModule from './modules/field-management';
 
 const modules = [
-     authModule,
-     userManagementModule,
-     organizationModule,
-     fieldManagementModule,
+    authModule,
+    userManagementModule,
+    organizationModule,
+    fieldManagementModule,
 ];
 
-export const createApp = () => {
-     const app = express();
-     app.set('trust proxy', true);
-     app.use(
-          cors({
-               origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:4000', 'https://api.asset.dev.client.kloudlite.io'],
-               credentials: true,
-          })
-     );
-     app.use(express.json({ limit: '250mb' }));
-     app.use(cookieParser());
+const origins = ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:4000', 'https://api.asset.dev.client.kloudlite.io'];
 
-     app.use(bodyParser.urlencoded({ extended: true }));
-     return app;
+export const createApp = () => {
+    const app = express();
+    app.set('trust proxy', true);
+    app.use(
+        cors({
+            credentials: true,
+            origin: (origin, callback) => {
+                const valid = origins.find((o) => o === origin);
+                console.log('valid', valid);
+                if (valid) {
+                    callback(undefined, valid);
+                } else {
+                    callback(new Error('no valid origin'), undefined);
+                }
+            }
+        })
+    );
+    app.use(express.json({limit: '250mb'}));
+    app.use(cookieParser());
+
+    app.use(bodyParser.urlencoded({extended: true}));
+    return app;
 };
 
 export const finishApp = (app) => {
-     app.use(notFoundHandler);
-     app.use(errorHandler);
+    app.use(notFoundHandler);
+    app.use(errorHandler);
 };
 
 export const useModules = (app) => {
-     modules.map((module) => module.init(app));
+    modules.map((module) => module.init(app));
 };
