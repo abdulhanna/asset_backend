a11dc92441005437ce94a01ceb525ab9558762fd
Pushpa Singh <pushpa@plaxonic.com>
2023-09-15 07:15:16 +0000

    Update app.js, set origin

--- src/app.js
+++ src/app.js
@@ -15,22 +15,20 @@ const modules = [
     fieldManagementModule,
 ];
 
-// const origins = ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:4000', 'https://api.asset.dev.client.kloudlite.io'];
-
 export const createApp = () => {
-    const app = express();
-    app.set('trust proxy', true);
-    app.use(
-        cors({
-            credentials: true,
-            origin: '*'
-        })
-    );
-    app.use(express.json({limit: '250mb'}));
-    app.use(cookieParser());
+     const app = express();
+     app.set('trust proxy', true);
+     app.use(
+          cors({
+               origin: ['http://localhost:3000', 'http://localhost:4000', 'https://api.asset.dev.client.kloudlite.io'],
+               credentials: true,
+          })
+     );
+     app.use(express.json({ limit: '250mb' }));
+     app.use(cookieParser());
 
-    app.use(bodyParser.urlencoded({extended: true}));
-    return app;
+     app.use(bodyParser.urlencoded({ extended: true }));
+     return app;
 };
 
 export const finishApp = (app) => {
