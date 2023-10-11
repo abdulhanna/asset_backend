6bcf9bc15cef11cffc18cc39dbe96c4aaeb3cfb8
Pushpa Singh <pushpa@plaxonic.com>
2023-10-11 15:22:13 +0530

    :sparkles: master table module modified and added

--- src/modules/master-table/models/masterTable.js
+++ src/modules/master-table/models/masterTable.js
@@ -1,55 +1,43 @@
 import mongoose from 'mongoose';
 
-const dynamicTableFieldSchema = new Schema({
-    fieldName: {
-        type: String,
-        required: true
-    },
-    fieldType: {
-        type: String,
-        enum: ['AlphaNumeric', 'TextFields', 'Numeric'],
-        required: true
-    },
-    rateType: {
-        type: String,
-        enum: ['SLM', 'WDV', null],
-        default: null
-    }
-});
-
-
 const masterTableSchema = new mongoose.Schema(
     {
-        tableID: {
+    tableId: {
             type: String,
+            ref: 'mastertablecolumns',
             required: true,
-            unique: true,
-        },
-        tableName: {
-            type: String,
-            required: true,
-        },
-        applicableTo: {
-            type: String,
-            enum: ['All', 'Organization', 'Country'],
+    },
+    masterTableData: [],
+        addedByUserId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'users',
             required: true
         },
-        applicableId: {
-            type: String,
-            default: null
+        isDeleted: {
+            type: Boolean,
+            default: false,
         },
-        fields: [dynamicTableFieldSchema],
-        uploadDocument: {
-            type: String
-        }
-    },
-    {
-        timestamps: true
-    }
-);
+        isDeactivated: {
+            type: Boolean,
+            default: false,
+        },
+        deletedAt: {
+            type: Date,
+            default: null,
+        },
+        createdAt: {
+            type: Date,
+            default: null,
+        },
+        updatedAt: {
+            type: Date,
+            default: null,
+        },
+},
+    { new: true }
+)
 
 
 const masterTableModel = mongoose.model('mastertables', masterTableSchema);
 
 export default masterTableModel;
-
--- src/modules/master-table/router/masterTable.js
+++ src/modules/master-table/router/masterTable.js
@@ -1,15 +1,30 @@
 import { Router } from 'express';
 import { httpHandler } from '@madhouselabs/http-helpers';
 import  {isLoggedIn} from "../../auth/router/passport";
+import masterTableColumnService from "../services/masterTableColumn";
 
 const router = Router();
 
-router.get("/all",
+
+router.post("/add",
     isLoggedIn,
-    httpHandler(async (req, res)=> {
-       console.log('ok')
+    httpHandler(async (req, res) => {
+        const userRole = req.user.data.role;
+        const organizationId = req.user.data.organizationId;
+        const result = await masterTableColumnService.createMasterTable(req.body, userRole, organizationId);
+        res.send(result);
     })
     )
 
 
-export default router();
+router.get("/all",
+    isLoggedIn,
+    httpHandler(async (req, res)=> {
+        const userId = req.user.data._id;
+        const result = await masterTableColumnService.getallTable(userId);
+        res.send(result)
+    })
+    )
+
+
+export default router;
