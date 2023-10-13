e73908bfa20b863635e74469e27fafd688d01104
Dhananjay Kumar <dhananjay@plaxonic.com>
2023-10-13 12:01:27 +0530

    Add api to update step form

--- src/modules/field-management/services/assetFormStep.js
+++ src/modules/field-management/services/assetFormStep.js
@@ -3,7 +3,7 @@ import fieldManagementModel from '../models/fieldManagement';
 
 const associateAssetFormStepWithGroups = async (stepNo, stepName, groups) => {
     try {
-        const newAssetFormStep = new assetFormStepModel({stepNo, stepName});
+        const newAssetFormStep = new assetFormStepModel({stepNo, stepName, createdAt: new Date()});
         const assetFormStep = await newAssetFormStep.save();
 
         if (assetFormStep) {
@@ -32,7 +32,80 @@ const associateAssetFormStepWithGroups = async (stepNo, stepName, groups) => {
     }
 };
 
+const listForms = async () => {
+    try {
+        const forms = await assetFormStepModel.find({
+            isDeleted: false
+        });
 
+        const formsWithGroupCount = await Promise.all(forms.map(async (form) => {
+            const formCount = await fieldManagementModel.countDocuments({
+                assetFormStepId: form._id
+            });
+            const formObject = form.toObject(); // Use form.toObject() to create a mutable object
+            formObject.groupsCount = formCount;
+            return formObject;
+        }));
+
+        return formsWithGroupCount;
+    } catch (error) {
+        throw error;
+    }
+};
+
+const updateForm = async (formId, stepNo, stepName, groups) => {
+    try {
+        const updatedForm = await assetFormStepModel.findByIdAndUpdate(formId, {stepNo, stepName}, {new: true});
+
+        if (updatedForm) {
+            const promises = groups.map(async group => {
+                const {groupId, orderNo} = group;
+
+                const foundGroup = await fieldManagementModel.findById(groupId);
+
+                if (foundGroup) {
+                    foundGroup.assetFormStepId = updatedForm._id;
+                    foundGroup.orderNo = orderNo;
+                    await foundGroup.save();
+                } else {
+                    throw new Error(`Group with ID ${groupId} not found`);
+                }
+            });
+
+            await Promise.all(promises);
+        } else {
+            throw new Error(`Form with ID ${formId} not found`);
+        }
+    } catch (error) {
+        throw error;
+    }
+};
+
+
+const deleteForm = async (formId) => {
+    try {
+        const deletedForm = await assetFormStepModel.findByIdAndDelete(formId);
+
+        if (!deletedForm) {
+            throw new Error(`Form with ID ${formId} not found`);
+        }
+
+        const associatedGroups = await fieldManagementModel.find({assetFormStepId: formId});
+
+        const promises = associatedGroups.map(async group => {
+            group.assetFormStepId = null;
+            group.orderNo = null;
+            await group.save();
+        });
+
+        await Promise.all(promises);
+    } catch (error) {
+        throw error;
+    }
+};
 export const assetFormStepService = {
-    associateAssetFormStepWithGroups
+    associateAssetFormStepWithGroups,
+    listForms,
+    updateForm,
+    deleteForm
 };
