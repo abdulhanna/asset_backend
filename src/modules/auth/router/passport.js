d5a411b05f0d0c0f09b62a41e45f4b0edd961608
Pushpa Singh <pushpa@plaxonic.com>
2023-11-06 16:48:16 +0530

    :recycle: cookie manaher code

--- src/modules/auth/router/passport.js
+++ src/modules/auth/router/passport.js
@@ -5,33 +5,11 @@ import userModel from '../models';
 
 // Function to extract JWT from cookies
 
-/***** Old code */
-// const extractJwtFromCookies = (req) => {
-//      const cookieName = 'access_token'; // Replace this with the actual name of your JWT cookie
-//      const cookieValue = req.cookies[cookieName];
-//      return cookieValue || null;
-//    };
-
-/***** new code */
-   const extractJwtFromCookies = (req) => {
-    const cookieName = 'access_token'; // Replace this with the actual name of your JWT cookie
-    const cookieValue = req.cookies[cookieName];
-  
-    if (cookieValue) {
-      // Check if the token is still valid in the schema
-      userModel.findOne({ token: cookieValue }, (err, user) => {
-        if (err || !user) {
-          // Token is invalid or user not found, remove the cookie
-          res.clearCookie(cookieName);
-          return null;
-        }
-      });
-  
-      return cookieValue;
-    }
-  
-    return null;
-  };
+const extractJwtFromCookies = (req) => {
+     const cookieName = 'access_token'; // Replace this with the actual name of your JWT cookie
+     const cookieValue = req.cookies[cookieName];
+     return cookieValue || null;
+   };
    
    const opts = {};
    
@@ -41,11 +19,34 @@ import userModel from '../models';
    opts.secretOrKey = secret.JWT_KEY;
    
    export const initPassport = () => {
+
+     /** old code */
+    //  passport.use(
+    //    new JwtStrategy(opts, (payload, done) => {
+    //     console.log(payload.data._id+'>>>>>>');
+    //      return done(null, payload);
+    //    })
+    //  );
+
+
+
+/**** new code */
      passport.use(
-       new JwtStrategy(opts, (payload, done) => {
-         return done(null, payload);
-       })
-     );
+      new JwtStrategy(opts, (payload, done) => {
+        // Query the database to check if the user and token are valid
+        userModel.findById(payload.data._id, (err, user) => {
+          if (err) {
+            return done(err, false);
+          }
+       // if token is not null
+          if (user && user.token !== null ) {
+            return done(null, payload);
+          } else {
+            return done(null, false);
+          }
+        });
+      })
+    );
    };
    
    export const isLoggedIn = (req, res, next) => {
